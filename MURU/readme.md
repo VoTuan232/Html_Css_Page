16cf7408fdd5c5bad7994bc8b61c5ac47b57f8d2
Tuan Vo <tuan.vo@sotatek.com>
2020-12-14 09:00:41 +0700

    feat-1: add-webpack

---
+++ MURU/readme.md
@@ -0,0 +1,5 @@
+- Link Design: https://app.zeplin.io/project/5f73bd3bd8a80a48a93d9344/screen/5f82c5de0fa62b757a676c4f
+
+- Tech:
+  + CSS,HTML
+  + WEBPACK
---
+++ MURU/webpack.config.js
@@ -0,0 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  plugins: [
+    new CleanWebpackPlugin({ cleanStaleWebpackAssets: false }),
+    new HtmlWebpackPlugin({
+      title: "Development",
+      template: "./src/index.html",
+    }),
+  ],
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [
+          "style-loader", //3. Inject styles into DOM
+          "css-loader", //2. Turns css into commonjs
+          "sass-loader", //1. Turns sass into css
+        ],
+      },
+    ],
+  },
+};
